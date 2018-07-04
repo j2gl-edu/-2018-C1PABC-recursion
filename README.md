72e6269d21173d9fc14839e83472611220440211
Juan J. Garcia <j2gl.edu@gmail.com>
2018-07-04 16:58:49 -0600

    [solution] Cleanup code

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# Unit 3 - Recursive
+# Unit 3 - Recursion
 
 This is an example for recursion.
 
 ## Required
 
-* jdk 1.8
+* jdk 1.8+
 * Maven - https://maven.apache.org/
 
 ## Build and Run
 
 ```sh
 mvn clean package
-java -jar target/recursive-examples-1.0.0-SNAPSHOT.jar
+java -jar target/recursion-examples-1.0.0-SNAPSHOT.jar
 ```
--- src/main/java/edu/advanced/recursion/MainRecursion.java
+++ src/main/java/edu/advanced/recursion/MainRecursion.java
@@ -4,10 +4,15 @@ public class MainRecursion {
 
     public static void main(String[] args) {
         System.out.println("Hello Recursion!");
+        System.out.println("");
 
         System.out.println("Factorial: ");
-        int number = 9;
+        int number = 5;
         System.out.println(number + "! = " + Factorial.doFactorial(number));
+        System.out.println("");
+
+        System.out.println("Hanoi Towers: ");
+        Hanoi.doHanoi(3, "A", "C", "B");
     }
     
 }
