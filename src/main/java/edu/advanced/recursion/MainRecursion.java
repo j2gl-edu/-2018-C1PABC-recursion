603f51e49fc008eb3e8498795a399c390c774338
Juan J. Garcia <j2gl.edu@gmail.com>
2018-07-04 16:52:28 -0600

    [solution] Rename to recursion

---
+++ src/main/java/edu/advanced/recursion/MainRecursion.java
@@ -0,0 +1,14 @@
+package edu.advanced.recursion;
+
+public class MainRecursion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Recursion!");
+
+        System.out.println("Factorial: ");
+        int number = 9;
+        System.out.println(number + "! = " + Factorial.doFactorial(number));
+    }
+    
+}
+  
